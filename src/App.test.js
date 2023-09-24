10a8df4add1443a6c700e7f0a6f0b2de5041eb54
Fabio <fabio.ferreira@tecnofit.com.br>
2023-09-24 19:32:01 -0300

    pipline fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import App from './App';
 
 test('renders devops pipeline text', () => {
   render(<App />);
-  const textElement = screen.getByText('Trabalho para a matéria DevOps');
-  expect(textElement).toBeInTheDocument();
+  const textElement1 = screen.getByText('Trabalho para a matéria DevOps');
   const textElement2 = screen.getByText('Pipeline de teste');
+  expect(textElement1).toBeInTheDocument();
   expect(textElement2).toBeInTheDocument();
 });
