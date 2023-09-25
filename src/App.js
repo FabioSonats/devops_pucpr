09343c806df0a835413f2977ef1b3f9a209caba0
Fabio <fabio.ferreira@tecnofit.com.br>
2023-09-25 18:10:28 -0300

    new pipeline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import './App.css'; // Importa o arquivo App.css
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [imc, setImc] = useState(null);
+
+  const calculateIMC = () => {
+    const weightInKg = parseFloat(weight);
+    const heightInMeters = parseFloat(height) / 100; // Convertendo altura de cm para metros
+
+    if (!isNaN(weightInKg) && !isNaN(heightInMeters) && heightInMeters > 0) {
+      const imcValue = weightInKg / (heightInMeters * heightInMeters);
+      setImc(imcValue.toFixed(2)); // Limitando o resultado a duas casas decimais
+    } else {
+      setImc(null);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +26,28 @@ function App() {
           <br />
           Pipeline de teste
         </p>
+        <div>
+          <label>Peso (kg):</label>
+          <input
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Altura (cm):</label>
+          <input
+            type="number"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+        <button onClick={calculateIMC}>Calcular IMC</button>
+        {imc !== null && (
+          <p>
+            Seu IMC é: <strong>{imc}</strong>
+          </p>
+        )}
       </header>
     </div>
   );
